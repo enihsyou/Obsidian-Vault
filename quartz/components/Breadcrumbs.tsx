55d4c7cbfad1d66b8a92856af92a301a663ec41f
九条涼果 <enihsyou@gmail.com>
2026-06-09 23:57:41 +0800

    fix: 短横线不在 breadcrumb 中显示的问题

--- quartz/components/Breadcrumbs.tsx
+++ quartz/components/Breadcrumbs.tsx
@@ -37,7 +37,7 @@ const defaultOptions: BreadcrumbOptions = {
 
 function formatCrumb(displayName: string, baseSlug: FullSlug, currentSlug: SimpleSlug): CrumbData {
   return {
-    displayName: displayName.replaceAll("-", " "),
+    displayName: displayName,
     path: resolveRelative(baseSlug, currentSlug),
   }
 }
